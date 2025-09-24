80d8e70ec3c2681324e1aa7f66383430894d0815
Migz <juanmiguelganfernandez@gmail.com>
2025-09-24 18:16:57 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
+
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
+import SpinnerFullPage from "./components/SpinnerFullPage";
+
+// import Homepage from "./pages/Homepage";
+// import Product from "./pages/Product";
+// import Pricing from "./pages/Pricing";
+// import Login from "./pages/Login";
+// import AppLayout from "./pages/AppLayout";
+// import PageNotFound from "./pages/PageNotFound";
+
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Login = lazy(() => import("./pages/Login"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+// dist/assets/index-498d1162.css   30.35 kB │ gzip:   5.08 kB
+// dist/assets/index-afc88e59.js   509.98 kB │ gzip: 148.87 kB
+
+function App() {
+  return (
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              {/* 👇 Actual route definition */}
+              {/* / - root path */}
+              {/* * - catch routes not matched to other paths */}
+              <Route path="/" element={<Homepage />} />
+              <Route path="product" element={<Product />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="login" element={<Login />} />
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Navigate replace to="cities" />} />
+                <Route path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
+  );
+}
+
+export default App;
+
+/* Routing with react-router
+1. Create components for pages 
+2. Install react router package - npm i react-router-dom
+3. Define routes
+4. Use Link and NavLink component provided by react-router-dom package to seamlessly transition between pages. No more reloads! */
+
+// 💡 NavLink is used in nav elements to indicate which is currently visited page
+
+// 💡 Instead of path="/", can also use index for homepage
+
+// To use params with React Router, we basically do it in 3 steps. First we create a new route,
+// then we link to that route, and then in that route we read the state from the URL.
