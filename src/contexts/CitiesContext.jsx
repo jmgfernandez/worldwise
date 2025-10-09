b9c5c7edfabacf4305f95960a9f2a2cea974a6da
Migz <juanmiguelganfernandez@gmail.com>
2025-10-09 21:03:00 +0800

    chore: update BASE_URL to use live Render API

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,7 +6,7 @@ import {
   useCallback,
 } from "react";
 
-const BASE_URL = "http://localhost:9000";
+const BASE_URL = "https://worldwise-api-wpyy.onrender.com/api";
 
 const CitiesContext = createContext();
 
